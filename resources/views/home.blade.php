20212f7d1001fb7e94495fb1a8ec8b729a9277a9
Neo-Machina <andreistatache1997@gmail.com>
2022-08-29 19:12:32 +0200

    html populeted & styling added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('main_content')
+    <h1>Lista viaggi</h1>
+
+    <div class="row">
+        @foreach ($travels as $travel)
+            <div class="col">
+                <div class="card">
+                    <div>Holiday type: {{ $travel->holiday_type }} </div>
+                    <div>Hotel: {{ $travel->hotel }}</div>
+                    <div>Price: {{ $travel->price }}</div>
+                    <br>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
+
+@section('page_title')
+    Homepage
+@endsection
